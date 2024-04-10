e03d496d7440e657a9748df1f58c2d53c2a63865
Matt Morley <matthew.morley.ca@gmail.com>
2024-04-10 10:02:52 -0400

    bjork stuffs

--- src/test_fake_tags.cpp
+++ src/test_fake_tags.cpp
@@ -17,9 +17,54 @@ using namespace std;
 using namespace gtsam;
 using symbol_shorthand::P;
 using symbol_shorthand::X;
+using symbol_shorthand::L;
 
 // Make the typename short so it looks much cleaner
 typedef SmartProjectionPoseFactor<Cal3_S2> SmartFactor;
+using LandmarkMap = map<Key, SmartFactor::shared_ptr>;
+
+constexpr int NUM_CORNERS = 4;
+
+gtsam::Key TagIdToKey(int tagID, int corner) {
+  return L(NUM_CORNERS * tagID + corner);
+}
+
+struct TagCorner {
+  Key landmarkID;
+  Point3 worldToCorner;
+
+  TagCorner(int tagID, int cornerID, Point3 p) : worldToCorner(p), landmarkID(TagIdToKey(tagID, cornerID)) {}
+};
+
+void addFakeObservations(LandmarkMap &landmarks, Key state, vector<TagCorner> &tagCorners, Pose3 &pose, Cal3_S2::shared_ptr K, Pose3 bodyToCamera) {
+  for (const auto& p : tagCorners) {
+    auto landmarkPair = landmarks.find(p.landmarkID);
+    if (landmarkPair != landmarks.end()) {
+      auto worldToCam = pose.compose(bodyToCamera);
+      auto relative = worldToCam.transformTo(p.worldToCorner);
+      cout 
+        << "observation " 
+        << worldToCam.x() 
+        << "," << worldToCam.y()
+        << "," << worldToCam.z()
+        << " landmark " 
+        << p.worldToCorner.x() 
+        << "," << p.worldToCorner.y()
+        << "," << p.worldToCorner.z()
+        << " relative " 
+        << relative.x() 
+        << "," << relative.y()
+        << "," << relative.z() << endl;
+      
+
+      auto measurement = PinholePose<Cal3_S2>(worldToCam, K).project(p.worldToCorner);
+      cout << "State " << state << ": landmark " << p.landmarkID << " fake-observed at " << measurement.x() << "," << measurement.y() << endl;
+      landmarkPair->second->add(measurement, state);
+    } else {
+      cerr << "Factor " << p.landmarkID << " not found in landmark map?" << endl;
+    }
+  }
+}
 
 /**
  * This test does a couple things:
@@ -31,7 +76,8 @@ typedef SmartProjectionPoseFactor<Cal3_S2> SmartFactor;
  * - at each timestamp we observe a fixed point in space through our camera
 */
 int main(int argc, char* argv[]) {
-  Cal3_S2::shared_ptr K(new Cal3_S2(1000, 1000, 0.0, 320, 240));
+  // approx a 1080p c920
+  Cal3_S2::shared_ptr K(new Cal3_S2(1300, 1300, 0, 1000, 500));
 
   auto measurementNoise =
       noiseModel::Isotropic::Sigma(2, 1.0);  // one pixel in u and v
@@ -40,9 +86,9 @@ int main(int argc, char* argv[]) {
   sigmas << Vector3::Constant(0.1), Vector3::Constant(0.3);
   auto posePriorNoise = noiseModel::Diagonal::Sigmas(sigmas);
 
-  // same odometry noise 
+  // odometry noise 
   Vector6 odomSigma;
-  odomSigma << Vector3::Constant(0.01), Vector3::Constant(0.05);
+  odomSigma << Vector3::Constant(0.001), Vector3::Constant(0.05);
   auto odometryNoise = noiseModel::Diagonal::Sigmas(odomSigma);
 
   ISAM2Params parameters;
@@ -57,17 +103,25 @@ int main(int argc, char* argv[]) {
   NonlinearFactorGraph graph;
   Values initialEstimate;
 
-  Point3 point(0.0, 0.0, 1.0);
+  vector<TagCorner> tagCorners {
+    {1, 0, {1, 2, 0}},
+    // {1, 0, {1, 1, 1}},
+    // {1, 1, {0, 0, 1}},
+    // {1, 2, {0.1, 0, 1}},
+    // {1, 3, {0, 0.1, 1}},
+    // {2, 0, {0, 2, 0}},
+  };
 
   // Intentionally initialize the variables off from the ground truth
   Pose3 delta(Rot3::Rodrigues(0.0, 0.0, 0.0), Point3(0.05, -0.10, 0.20));
 
   // Ground truth poses for our robot's trajectory
+  // we strafe in +y (so left in the robot frame)
   Pose3 pose1(Rot3(), Point3(0.0, 0.0, 0.0));
-  Pose3 pose2(Rot3(), Point3(0.0, 0.2, 0.0));
-  Pose3 pose3(Rot3(), Point3(0.0, 0.4, 0.0));
-  Pose3 pose4(Rot3(), Point3(0.0, 0.5, 0.0));
-  Pose3 pose5(Rot3(), Point3(0.0, 0.6, 0.0));
+  Pose3 pose2(Rot3(), Point3(0, .2, 0.0));
+  Pose3 pose3(Rot3(), Point3(0, .4, 0.0));
+  Pose3 pose4(Rot3(), Point3(0, .5, 0.0));
+  Pose3 pose5(Rot3(), Point3(0, .6, 0.0));
 
   vector<Pose3> poses = {pose1, pose2, pose3, pose4, pose5};
 
@@ -75,11 +129,21 @@ int main(int argc, char* argv[]) {
   graph.addPrior(X(0), poses[0], posePriorNoise);
   initialEstimate.insert(X(0), poses[0].compose(delta));
 
+  // body-camera transform
+  // Camera +Z points out of the camera, so rotate camera 90 about +x so we have a camera pointing out the left of the robot
+  Pose3 body_P_sensor {Rot3::Rx(-90 * 3.141592 / 180.0), Point3{}};
+
+  // Each landmark we can observe multiple times needs its own smartfactor
+  LandmarkMap landmarks;
+  for (const auto& p : tagCorners) {
+    landmarks[p.landmarkID] = SmartFactor::shared_ptr(new SmartFactor(measurementNoise, K, body_P_sensor));
+
+    // Not sure if this is legal to go before adding the first factor?
+    graph.push_back(landmarks[p.landmarkID]);
+  }
+
   // Create smart factor with measurement from first pose only
-  Pose3 body_P_sensor {Rot3{}, Point3{}};
-  SmartFactor::shared_ptr smartFactor(new SmartFactor(measurementNoise, K, body_P_sensor));
-  smartFactor->add(PinholePose<Cal3_S2>(poses[0], K).project(point), X(0));
-  graph.push_back(smartFactor);
+  addFakeObservations(landmarks, X(0), tagCorners, poses[0], K, body_P_sensor);
 
   // loop over remaining poses
   for (size_t i = 1; i < 5; i++) {
@@ -94,43 +158,41 @@ int main(int argc, char* argv[]) {
     initialEstimate.insert(X(i), poses[i].compose(delta));
 
 
-    // "Simulate" measurement from this pose
-    PinholePose<Cal3_S2> camera(poses[i], K);
-    Point2 measurement = camera.project(point);
-    cout << "Measurement " << i << ": [" << measurement << "]" << endl;
-
-    // Add measurement to smart factor
-    smartFactor->add(measurement, X(i));
+    // "Simulate" measurements from this pose
+    addFakeObservations(landmarks, X(i), tagCorners, poses[i], K, body_P_sensor);
 
     // Update iSAM2
     ISAM2Result result = isam.update(graph, initialEstimate);
     cout << "Isam2 results: ";
     result.print();
 
-    cout << "> Detailed results:" << endl;
-    for (auto keyedStatus : result.detail->variableStatus) {
-      const auto& status = keyedStatus.second;
-      PrintKey(keyedStatus.first);
-      cout << " {" << endl;
-      cout << "reeliminated: " << status.isReeliminated << endl;
-      cout << "relinearized above thresh: " << status.isAboveRelinThreshold
-           << endl;
-      cout << "relinearized involved: " << status.isRelinearizeInvolved << endl;
-      cout << "relinearized: " << status.isRelinearized << endl;
-      cout << "observed: " << status.isObserved << endl;
-      cout << "new: " << status.isNew << endl;
-      cout << "in the root clique: " << status.inRootClique << endl;
-      cout << "}" << endl;
-    }
+    // cout << "> Detailed results:" << endl;
+    // for (auto keyedStatus : result.detail->variableStatus) {
+    //   const auto& status = keyedStatus.second;
+    //   PrintKey(keyedStatus.first);
+    //   cout << " {" << endl;
+    //   cout << "reeliminated: " << status.isReeliminated << endl;
+    //   cout << "relinearized above thresh: " << status.isAboveRelinThreshold
+    //        << endl;
+    //   cout << "relinearized involved: " << status.isRelinearizeInvolved << endl;
+    //   cout << "relinearized: " << status.isRelinearized << endl;
+    //   cout << "observed: " << status.isObserved << endl;
+    //   cout << "new: " << status.isNew << endl;
+    //   cout << "in the root clique: " << status.inRootClique << endl;
+    //   cout << "}" << endl;
+    // }
 
     Values currentEstimate = isam.calculateEstimate();
     currentEstimate.print("Current estimate: ");
 
-    boost::optional<Point3> pointEstimate = smartFactor->point(currentEstimate);
-    if (pointEstimate) {
-      cout << *pointEstimate << endl;
-    } else {
-      cout << "Point degenerate." << endl;
+    for (auto [key, smartFactor] : landmarks) {
+      cout << "Landmark " << key << " point estimate" << endl;
+      boost::optional<Point3> pointEstimate = smartFactor->point(currentEstimate);
+      if (pointEstimate) {
+        cout << *pointEstimate << endl;
+      } else {
+        cout << "Point degenerate." << endl;
+      }
     }
 
     // Reset graph and initial estimate for next iteration
