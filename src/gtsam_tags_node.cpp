01ab4d9f8f012d9064296ec05ccecfaae7f5dfb9
Matt <matthew.morley.ca@gmail.com>
2024-04-30 00:21:46 -0400

    Add back in old code

--- src/gtsam_tags_node.cpp
+++ src/gtsam_tags_node.cpp
@@ -40,6 +40,9 @@
 #include "gtsam_utils.h"
 #include "localizer.h"
 
+using namespace gtsam;
+using std::vector;
+
 // Hard-coded for now -- TODO move to configurable
 const Pose3 bodyPcamera_cam1{/*
                         We want:
@@ -77,182 +80,180 @@ static Localizer CreateLocalizer(CameraConfig config, Pose3 bodyPcamera,
 int main(int argc, char **argv) {
   using namespace std::chrono_literals;
 
-  LocalizerConfig config = ParseConfig("test/resources/good_config.json");
-  config.print();
+  // LocalizerConfig config = ParseConfig("test/resources/good_config.json");
+  // config.print();
 
   nt::NetworkTableInstance inst = nt::NetworkTableInstance::GetDefault();
   inst.StopServer();
   inst.SetServer("192.168.1.226");
   inst.StartClient4("gtsam-meme");
 
-  // // Attach listener
-  // nt::StructArrayTopic<TagDetection> tagTopic =
-  //     inst.GetStructArrayTopic<TagDetection>("/cam/tags");
-  // auto tagSub = tagTopic.Subscribe({}, {
-  //                                          .pollStorage = 100,
-  //                                          .sendAll = true,
-  //                                          .keepDuplicates = true,
-  //                                      });
+  // Attach listener
+  nt::StructArrayTopic<TagDetection> tagTopic =
+      inst.GetStructArrayTopic<TagDetection>("/cam/tags");
+  auto tagSub = tagTopic.Subscribe({}, {
+                                           .pollStorage = 100,
+                                           .sendAll = true,
+                                           .keepDuplicates = true,
+                                       });
 
-  // nt::StructTopic<frc::Twist3d> odomTopic =
-  //     inst.GetStructTopic<frc::Twist3d>("/robot/odom");
-  // auto odomSub = odomTopic.Subscribe({}, {
-  //                                            .pollStorage = 100,
-  //                                            .sendAll = true,
-  //                                            .keepDuplicates = true,
-  //                                        });
+  nt::StructTopic<frc::Twist3d> odomTopic =
+      inst.GetStructTopic<frc::Twist3d>("/robot/odom");
+  auto odomSub = odomTopic.Subscribe({}, {
+                                             .pollStorage = 100,
+                                             .sendAll = true,
+                                             .keepDuplicates = true,
+                                         });
 
-  // auto poseEstimatePub =
-  //     inst.GetDoubleArrayTopic(
-  //             "/SmartDashboard/VisionSystemSim-main/Sim Field/Gtsam Robot")
-  //         .Publish({.sendAll = true, .keepDuplicates = true});
-  // auto observedCornersPub =
-  //     inst.GetDoubleArrayTopic("/cam/gtsam_seen_corners")
-  //         .Publish({.sendAll = true, .keepDuplicates = true});
-  // auto predictedCornersPub =
-  //     inst.GetDoubleArrayTopic("/cam/gtsam_predicted_corners")
-  //         .Publish({.sendAll = true, .keepDuplicates = true});
-  // auto trajectoryPub =
-  // inst.GetStructArrayTopic<frc::Pose3d>("/cam/gtsam_traj")
-  //                          .Publish({.sendAll = true, .keepDuplicates =
-  //                          true});
-  // auto dtPublisher = inst.GetDoubleTopic("/cam/update_dt_ms").Publish();
-  // // standard deviations on rx ry rz tx ty tz
-  // auto stdevPub = inst.GetDoubleArrayTopic("/cam/std_dev").Publish();
+  auto poseEstimatePub =
+      inst.GetDoubleArrayTopic(
+              "/SmartDashboard/VisionSystemSim-main/Sim Field/Gtsam Robot")
+          .Publish({.sendAll = true, .keepDuplicates = true});
+  auto observedCornersPub =
+      inst.GetDoubleArrayTopic("/cam/gtsam_seen_corners")
+          .Publish({.sendAll = true, .keepDuplicates = true});
+  auto predictedCornersPub =
+      inst.GetDoubleArrayTopic("/cam/gtsam_predicted_corners")
+          .Publish({.sendAll = true, .keepDuplicates = true});
+  auto trajectoryPub = inst.GetStructArrayTopic<frc::Pose3d>("/cam/gtsam_traj")
+                           .Publish({.sendAll = true, .keepDuplicates = true});
+  auto dtPublisher = inst.GetDoubleTopic("/cam/update_dt_ms").Publish();
+  // standard deviations on rx ry rz tx ty tz
+  auto stdevPub = inst.GetDoubleArrayTopic("/cam/std_dev").Publish();
 
-  // // Assume robot isn't moving to get initial guess
+  // Assume robot isn't moving to get initial guess
 
-  // Pose3 initialEstimate;
-  // {
-  //   auto initialGuessSub =
-  //       inst.GetStructTopic<frc::Pose3d>("/robot/multi_tag_pose")
-  //           .Subscribe({}, {
-  //                              .pollStorage = 100,
-  //                              .sendAll = true,
-  //                              .keepDuplicates = true,
-  //                          });
+  Pose3 initialEstimate;
+  {
+    auto initialGuessSub =
+        inst.GetStructTopic<frc::Pose3d>("/robot/multi_tag_pose")
+            .Subscribe({}, {
+                               .pollStorage = 100,
+                               .sendAll = true,
+                               .keepDuplicates = true,
+                           });
 
-  //   while (true) {
-  //     auto guessArr = initialGuessSub.ReadQueue();
-  //     if (!guessArr.empty()) {
-  //       initialEstimate = FrcToGtsamPose3(guessArr.back().value);
-  //       initialEstimate.print("Initial guess");
-  //       break;
-  //     } else {
-  //       fmt::println("No initial guess yet, sleeping");
-  //       std::this_thread::sleep_for(200ms);
-  //     }
-  //   }
-  // }
+    while (true) {
+      auto guessArr = initialGuessSub.ReadQueue();
+      if (!guessArr.empty()) {
+        initialEstimate = FrcToGtsamPose3(guessArr.back().value);
+        initialEstimate.print("Initial guess");
+        break;
+      } else {
+        fmt::println("No initial guess yet, sleeping");
+        std::this_thread::sleep_for(200ms);
+      }
+    }
+  }
 
-  // Localizer localizer = CreateLocalizer(initialEstimate);
+  Localizer localizer = CreateLocalizer(initialEstimate);
 
-  // // hacky eventloop
-  // while (true) {
-  //   const auto start = std::chrono::steady_clock::now();
+  // hacky eventloop
+  while (true) {
+    const auto start = std::chrono::steady_clock::now();
 
-  //   const auto tags = tagSub.ReadQueue();
-  //   const auto odom = odomSub.ReadQueue();
+    const auto tags = tagSub.ReadQueue();
+    const auto odom = odomSub.ReadQueue();
 
-  //   for (auto o : odom) {
-  //     auto twist = o.value;
+    for (auto o : odom) {
+      auto twist = o.value;
 
-  //     Pose3 gtsamTwist{Rot3::Rodrigues(Vector3{
-  //                          twist.rx.to<double>(),
-  //                          twist.ry.to<double>(),
-  //                          twist.rz.to<double>(),
-  //                      }),
-  //                      Point3{twist.dx.to<double>(), twist.dy.to<double>(),
-  //                             twist.dz.to<double>()}};
+      Pose3 gtsamTwist{Rot3::Rodrigues(Vector3{
+                           twist.rx.to<double>(),
+                           twist.ry.to<double>(),
+                           twist.rz.to<double>(),
+                       }),
+                       Point3{twist.dx.to<double>(), twist.dy.to<double>(),
+                              twist.dz.to<double>()}};
 
-  //     try {
-  //       localizer.AddOdometry(gtsamTwist, units::microsecond_t{o.time});
-  //     } catch (std::exception e) {
-  //       fmt::println("whoops, {}", e.what());
-  //     }
-  //   }
+      try {
+        localizer.AddOdometry(gtsamTwist, units::microsecond_t{o.time});
+      } catch (std::exception e) {
+        fmt::println("whoops, {}", e.what());
+      }
+    }
 
-  //   for (const auto &tarr : tags) {
-  //     std::vector<double> corners;
-  //     std::vector<double> seenCorners;
-  //     // fmt::println("Update from {}:", tarr.time);
+    for (const auto &tarr : tags) {
+      std::vector<double> corners;
+      std::vector<double> seenCorners;
+      // fmt::println("Update from {}:", tarr.time);
 
-  //     for (const auto &t : tarr.value) {
-  //       {
-  //         vector<Point2> cornersForGtsam;
-  //         for (const auto &c : t.corners) {
-  //           cornersForGtsam.emplace_back(c.first, c.second);
-  //         }
+      for (const auto &t : tarr.value) {
+        {
+          vector<Point2> cornersForGtsam;
+          for (const auto &c : t.corners) {
+            cornersForGtsam.emplace_back(c.first, c.second);
+          }
 
-  //         try {
-  //           localizer.AddTagObservation(t.id, cornersForGtsam,
-  //                                       units::microsecond_t{tarr.time});
-  //         } catch (std::exception e) {
-  //           fmt::println("whoops tag, {}", e.what());
-  //         }
-  //       }
+          try {
+            localizer.AddTagObservation(t.id, cornersForGtsam,
+                                        units::microsecond_t{tarr.time});
+          } catch (std::exception e) {
+            fmt::println("whoops tag, {}", e.what());
+          }
+        }
 
-  //       // Publish reprojected corners
-  //       try {
-  //         auto worldPtag_opt = TagModel::WorldToCorners(t.id);
-  //         if (worldPtag_opt) {
-  //           auto worldPtag = worldPtag_opt.value();
-  //           auto worldTrobot = localizer.GetLatestWorldToBody();
-  //           PinholeCamera<Cal3_S2> cam(worldTrobot * bodyPcamera, K);
-  //           for (auto worldPcorner : worldPtag) {
-  //             const Point2 prediction = cam.project2(worldPcorner);
-  //             corners.push_back(prediction.x());
-  //             corners.push_back(prediction.y());
-  //           }
-  //           for (auto seenCorner : t.corners) {
-  //             seenCorners.push_back(seenCorner.first);
-  //             seenCorners.push_back(seenCorner.second);
-  //           }
-  //         }
-  //       } catch (std::exception e) {
-  //         fmt::println("whoops reproj, {}", e.what());
-  //       }
-  //     }
+        // Publish reprojected corners
+        try {
+          auto worldPtag_opt = TagModel::WorldToCorners(t.id);
+          if (worldPtag_opt) {
+            auto worldPtag = worldPtag_opt.value();
+            auto worldTrobot = localizer.GetLatestWorldToBody();
+            PinholeCamera<Cal3_S2> cam(worldTrobot * bodyPcamera, K);
+            for (auto worldPcorner : worldPtag) {
+              const Point2 prediction = cam.project2(worldPcorner);
+              corners.push_back(prediction.x());
+              corners.push_back(prediction.y());
+            }
+            for (auto seenCorner : t.corners) {
+              seenCorners.push_back(seenCorner.first);
+              seenCorners.push_back(seenCorner.second);
+            }
+          }
+        } catch (std::exception e) {
+          fmt::println("whoops reproj, {}", e.what());
+        }
+      }
 
-  //     predictedCornersPub.Set(corners, tarr.time);
-  //     observedCornersPub.Set(seenCorners, tarr.time);
-  //   }
+      predictedCornersPub.Set(corners, tarr.time);
+      observedCornersPub.Set(seenCorners, tarr.time);
+    }
 
-  //   // Update so we can publish a new pose estimate every tick
-  //   localizer.Optimize();
-  //   auto est = localizer.GetLatestWorldToBody();
-  //   auto rot = est.rotation().toQuaternion();
-  //   std::vector<double> poseEst{est.x(), est.y(), est.z(), rot.w(),
-  //                               rot.x(), rot.y(), rot.z()};
-  //   poseEstimatePub.Set(poseEst);
+    // Update so we can publish a new pose estimate every tick
+    localizer.Optimize();
+    auto est = localizer.GetLatestWorldToBody();
+    auto rot = est.rotation().toQuaternion();
+    std::vector<double> poseEst{est.x(), est.y(), est.z(), rot.w(),
+                                rot.x(), rot.y(), rot.z()};
+    poseEstimatePub.Set(poseEst);
 
-  //   {
-  //     // std::cout << "Latest mariginal\n" << localizer.GetLatestMarginals()
-  //     <<
-  //     // std::endl; std::cout << "Latest stdev\n" <<
-  //     // localizer.GetPoseComponentStdDevs() << std::endl;
-  //     auto mat = localizer.GetPoseComponentStdDevs();
-  //     std::vector<double> vec(mat.data(), mat.data() + mat.rows() *
-  //     mat.cols()); stdevPub.Set(vec);
-  //   }
+    {
+      // std::cout << "Latest mariginal\n" << localizer.GetLatestMarginals()
+      // <<
+          // std::endl; std::cout << "Latest stdev\n" <<
+          // localizer.GetPoseComponentStdDevs() << std::endl;
+          auto mat = localizer.GetPoseComponentStdDevs();
+      std::vector<double> vec(mat.data(), mat.data() + mat.rows() * mat.cols());
+      stdevPub.Set(vec);
+    }
 
-  //   const auto end = std::chrono::steady_clock::now();
-  //   double dt_ms =
-  //       std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(
-  //           end - start)
-  //           .count();
-  //   dtPublisher.Set(dt_ms);
+    const auto end = std::chrono::steady_clock::now();
+    double dt_ms =
+        std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(
+            end - start)
+            .count();
+    dtPublisher.Set(dt_ms);
 
-  //   static int i;
-  //   i++;
+    static int i;
+    i++;
 
-  //   if (i % 10 == 3)
-  //     trajectoryPub.Set(localizer.GetPoseHistory());
+    if (i % 10 == 3)
+      trajectoryPub.Set(localizer.GetPoseHistory());
 
-  //   inst.Flush();
+    inst.Flush();
 
-  //   std::this_thread::sleep_for(30ms);
-  // }
+    std::this_thread::sleep_for(30ms);
+  }
 
   return 0;
 }
