c0cb0893bd999a80f79b99e7f8a3708b3ffdca2e
Matt M <Matt M>
2024-08-12 07:34:10 -0700

    Attempt to cache initial Values (it's totally broken)

--- src/sfm_mapper/main.cpp
+++ src/sfm_mapper/main.cpp
@@ -36,6 +36,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <thread>
 #include <variant>
 #include <vector>
 
@@ -51,6 +52,7 @@
 #include "TagModel.h"
 #include "gtsam_utils.h"
 #include "helpers.h"
+#include "mapper_nt_iface.h"
 
 using std::cout;
 using std::endl;
@@ -65,6 +67,8 @@ static frc::AprilTagFieldLayout tagLayoutGuess =
     frc::LoadAprilTagLayoutField(frc::AprilTagField::k2024Crescendo);
 
 int main() {
+  MapperNtIface ntIface;
+
   // Camera calibration parameters. Order is [fx fy skew cx cy] in pixels
   Cal3_S2 K(cam_fx, cam_fy, 0.0, cam_cx, cam_cy);
   Isotropic::shared_ptr cameraNoise =
@@ -82,118 +86,141 @@ int main() {
   // constant Expression for the calibration we can reuse
   Cal3_S2_ cameraCal(K);
 
-  // Map of [observation state ID] to [tags seen]
-  map<Key, vector<TagDetection>> points = ParseFile();
+  while (true) {
 
-  // Create a factor graph
-  ExpressionFactorGraph graph;
+    // Map of [observation state ID] to [tags seen]
+    // map<Key, vector<TagDetection>> points = ParseFile();
+    auto points = ntIface.NewKeyframes();
 
-  // Add all our tag observations
-  for (const auto &[stateKey, tags] : points) {
-    for (const TagDetection &tag : tags) {
-      auto worldPcorners = TagModel::WorldToCornersFactor(L(tag.id));
+    if (!points.size()) {
+      cout << "no new keyframes - waiting";
 
-      // add each tag corner
-      constexpr int NUM_CORNERS = 4;
-      for (size_t i = 0; i < NUM_CORNERS; i++) {
-        // Decision variable - where our camera is in the world
-        const Pose3_ worldTbody_fac(stateKey);
-        // Where we'd predict the i'th corner of the tag to be
-        const auto prediction = PredictLandmarkImageLocationFactor(
-            worldTbody_fac, robotTcamera, cameraCal, worldPcorners[i]);
-        // where we saw the i'th corner in the image
-        Point2 measurement = {tag.corners[i].x, tag.corners[i].y};
-        // Add this prediction/measurement pair to our graph
-        graph.addExpressionFactor(prediction, measurement, cameraNoise);
+      using namespace std::chrono_literals;
+      std::this_thread::sleep_for(1000ms);
+    }
+
+    // Create a factor graph
+    ExpressionFactorGraph graph;
+
+    // Add all our tag observations
+    for (const auto &[stateKey, tags] : points) {
+      for (const TagDetection &tag : tags) {
+        auto worldPcorners = TagModel::WorldToCornersFactor(L(tag.id));
+
+        // add each tag corner
+        constexpr int NUM_CORNERS = 4;
+        for (size_t i = 0; i < NUM_CORNERS; i++) {
+          // Decision variable - where our camera is in the world
+          const Pose3_ worldTbody_fac(stateKey);
+          // Where we'd predict the i'th corner of the tag to be
+          const auto prediction = PredictLandmarkImageLocationFactor(
+              worldTbody_fac, robotTcamera, cameraCal, worldPcorners[i]);
+          // where we saw the i'th corner in the image
+          Point2 measurement = {tag.corners[i].x, tag.corners[i].y};
+          // Add this prediction/measurement pair to our graph
+          graph.addExpressionFactor(prediction, measurement, cameraNoise);
+        }
       }
     }
-  }
 
-  // Add pose prior on our fixed tag. Right now, foce it to be a tag in our
-  // dataset
-  int FIXED_TAG = 7;
-  auto worldTtag1 = TagModel::worldToTag(FIXED_TAG);
-  if (!worldTtag1) {
-    fmt::println("Couldnt find tag {} in map!", FIXED_TAG);
-  }
-  graph.addPrior(L(FIXED_TAG), *worldTtag1, posePriorNoise);
-
-  // Initial guess for our optimizer. Needs to be in the right ballpark, but
-  // accuracy doesn't super matter
-  Values initial;
-
-  // Guess for all camera poses
-  for (const auto &[stateKey, tags] : points) {
-    // Initial guess at camera pose based on regular old multi tag pose
-    // esitmation
-    auto worldTcam_guess = estimateObservationPose(tags, tagLayoutGuess);
-    if (!worldTcam_guess) {
-      std::cerr << "Can't guess pose of camera for observation " << stateKey
-                << std::endl;
-      ;
-    } else {
-      initial.insert<Pose3>(stateKey, *worldTcam_guess);
+    // Add pose prior on our fixed tag. Right now, foce it to be a tag in our
+    // dataset
+    int FIXED_TAG = 7;
+    auto worldTtag1 = TagModel::worldToTag(FIXED_TAG);
+    if (!worldTtag1) {
+      fmt::println("Couldnt find tag {} in map!", FIXED_TAG);
     }
-  }
+    graph.addPrior(L(FIXED_TAG), *worldTtag1, posePriorNoise);
 
-  // Guess for tag locations
-  for (const frc::AprilTag &tag : tagLayoutGuess.GetTags()) {
-    if (tagWasUsed(points, tag.ID)) {
-      // quick offset hack to disturb the pose by
-      Pose3 offset{Rot3::RzRyRx(0.12, -0.15, 0.02), Point3{0.5, -0.5, 1.1}};
-      initial.insert(L(tag.ID), Pose3dToGtsamPose3(tag.pose) * offset);
+    // Initial guess for our optimizer. Needs to be in the right ballpark, but
+    // accuracy doesn't super matter
+    Values initial;
+
+    // Guess for all camera poses
+    for (const auto &[stateKey, tags] : points) {
+      // Initial guess at camera pose based on regular old multi tag pose
+      // esitmation
+      auto worldTcam_guess = estimateObservationPose(tags, tagLayoutGuess);
+      if (!worldTcam_guess) {
+        std::cerr << "Can't guess pose of camera for observation " << stateKey
+                  << std::endl;
+        ;
+      } else {
+        initial.insert<Pose3>(stateKey, *worldTcam_guess);
+      }
     }
-  }
 
-  /* Optimize the graph and print results */
-  cout << "==========================\ninitial error = " << graph.error(initial)
-       << endl;
-  auto start = std::chrono::steady_clock::now();
-
-  DoglegParams params;
-  params.verbosity = NonlinearOptimizerParams::ERROR;
-  // params.relativeErrorTol = 1e-3;
-  // params.absoluteErrorTol = 1e-3;
-
-  // Create initial optimizer
-  DoglegOptimizer optimizer{graph, initial, params};
-
-  // Run full optimization until convergence.
-  Values result = optimizer.optimize();
-
-  auto end = std::chrono::steady_clock::now();
-  auto dt = end - start;
-  long long microseconds =
-      std::chrono::duration_cast<std::chrono::microseconds>(dt).count();
-
-  // The new optimizer has results and statistics
-  cout << "\n===== Converged in " << optimizer.iterations() << " iterations ("
-       << microseconds << " uS) with final error " << optimizer.error()
-       << " ======" << endl;
-
-  cout << "Results:" << endl;
-
-  {
-    gtsam::Marginals marginals(graph, result);
-    for (auto [key, value] : result) {
-      std::cout << "\n========= Key " << gtsam::Symbol(key) << " ==========\n";
-
-      // Assume all our keys are pose3 factors. lol.
-      auto est = GtsamToFrcPose3d(result.at<gtsam::Pose3>(key));
-      fmt::println("Estimated pose:");
-      fmt::println("Translation: x={:.2f} y={:.2f} z={:.2f}", est.X(), est.Y(),
-                   est.Z());
-      fmt::println("Rotation: W={:.3f} X={:.3f} Y={:.3f} Z={:.3f}",
-                   est.Rotation().GetQuaternion().W(),
-                   est.Rotation().GetQuaternion().X(),
-                   est.Rotation().GetQuaternion().Y(),
-                   est.Rotation().GetQuaternion().Z());
-
-      // Covariance is the variance of x_i with x_i - stddev is sqrt(var)
-      std::cout << "Marginal covariance (r t):" << endl
-                << marginals.marginalCovariance(key).diagonal().cwiseSqrt()
-                << std::endl;
+    // Guess for tag locations
+    for (const frc::AprilTag &tag : tagLayoutGuess.GetTags()) {
+      if (tagWasUsed(points, tag.ID)) {
+        // quick offset hack to disturb the pose by
+        Pose3 offset{Rot3::RzRyRx(0.12, -0.15, 0.02), Point3{0.5, -0.5, 1.1}};
+        initial.insert(L(tag.ID), Pose3dToGtsamPose3(tag.pose) * offset);
+      }
     }
+
+    /* Optimize the graph and print results */
+    cout << "==========================\ninitial error = "
+         << graph.error(initial) << endl;
+    auto start = std::chrono::steady_clock::now();
+
+    DoglegParams params;
+    params.verbosity = NonlinearOptimizerParams::ERROR;
+    // params.relativeErrorTol = 1e-3;
+    // params.absoluteErrorTol = 1e-3;
+
+    // Create initial optimizer
+    DoglegOptimizer optimizer{graph, initial, params};
+
+    // Run full optimization until convergence.
+    Values result = optimizer.optimize();
+
+    auto end = std::chrono::steady_clock::now();
+    auto dt = end - start;
+    long long microseconds =
+        std::chrono::duration_cast<std::chrono::microseconds>(dt).count();
+
+    // The new optimizer has results and statistics
+    cout << "\n===== Converged in " << optimizer.iterations() << " iterations ("
+         << microseconds << " uS) with final error " << optimizer.error()
+         << " ======" << endl;
+
+    cout << "Results:" << endl;
+
+    {
+      gtsam::Marginals marginals(graph, result);
+      std::vector<frc::AprilTag> tags;
+
+      for (auto [key, value] : result) {
+        std::cout << "\n========= Key " << gtsam::Symbol(key)
+                  << " ==========\n";
+
+        // Assume all our keys are pose3 factors. lol.
+        auto est = GtsamToFrcPose3d(result.at<gtsam::Pose3>(key));
+        fmt::println("Estimated pose:");
+        fmt::println("Translation: x={:.2f} y={:.2f} z={:.2f}", est.X(),
+                     est.Y(), est.Z());
+        fmt::println("Rotation: W={:.3f} X={:.3f} Y={:.3f} Z={:.3f}",
+                     est.Rotation().GetQuaternion().W(),
+                     est.Rotation().GetQuaternion().X(),
+                     est.Rotation().GetQuaternion().Y(),
+                     est.Rotation().GetQuaternion().Z());
+
+        // Covariance is the variance of x_i with x_i - stddev is sqrt(var)
+        std::cout << "Marginal covariance (r t):" << endl
+                  << marginals.marginalCovariance(key).diagonal().cwiseSqrt()
+                  << std::endl;
+
+        // todo - track all tag keys instead of this hack
+        if (key >= L(0) && key <= L(2000)) {
+          tags.push_back(frc::AprilTag{key - L(0), est});
+        }
+      }
+
+      frc::AprilTagFieldLayout layout {tags, 16.541,8.211};
+      ntIface.PublishLayout(layout);
+    }
+
   }
 
   return 0;
